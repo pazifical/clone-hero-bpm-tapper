ab06260f9d84f1fc927f4cbbc0d14cabb3fd9fcc
Paz <50664967+TwoWaySix@users.noreply.github.com>
2023-12-29 22:19:15 +0100

    bugfix of wrong logic

--- internal/bpm.go
+++ internal/bpm.go
@@ -1,7 +1,5 @@
 package internal
 
-import "fmt"
-
 type BPMPart struct {
 	T0       float64
 	T1       float64
@@ -22,27 +20,26 @@ func CalculateBPMParts(tapTimes []float64, chartInfo ChartInfo) []BPMPart {
 	r := 0
 	n := chartInfo.AverageBeatCount
 	factor := chartInfo.AverageBeatCount / chartInfo.BeatsPerBar
-	for i < len(tapTimes)-n+1 {
-		currentTapTimes := tapTimes[i:(i + n)]
-		fmt.Println(currentTapTimes)
+	for i < len(tapTimes)-n {
+		currentTapTimes := tapTimes[i:(i + n + 1)]
 
 		starts := currentTapTimes[0 : len(currentTapTimes)-1]
 		ends := currentTapTimes[1:]
 
 		dtSum := 0.
-		for j := 0; j < len(starts); j++ {
+		for j := 0; j < n; j++ {
 			dtSum += ends[j] - starts[j]
 		}
-		avg := dtSum / float64(len(starts))
+		avg := dtSum / float64(n)
 		bpm := 60. / avg
 		bpmParts = append(bpmParts, BPMPart{
-			T0:       tapTimes[i],
-			T1:       tapTimes[i+n-1],
+			T0:       starts[0],
+			T1:       ends[len(ends)-1],
 			BPM:      bpm,
 			Position: 192 * r * chartInfo.BeatsPerBar * factor,
 		})
 
-		i += (n - 1)
+		i += n
 		r += 1
 	}
 	return bpmParts
--- internal/bpm_test.go
+++ internal/bpm_test.go
@@ -7,46 +7,61 @@ import (
 
 func Test44WithAverage4(t *testing.T) {
 	tapTimes := []float64{
-		1, 2, 3, 4, 6, 8, 10, 12,
+		0.0, 0.5, 1.0, 1.5, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0,
 	}
 	chartInfo := ChartInfo{Name: "Name", Artist: "Artist", AverageBeatCount: 4, BeatsPerBar: 4}
 
 	bpmParts := CalculateBPMParts(tapTimes, chartInfo)
 	fmt.Println(bpmParts)
 
-	if bpmParts[0].BPM != 60. {
-		t.Errorf("want: %f != got: %f", 60., bpmParts[0].BPM)
+	want := 120.
+	got := bpmParts[0].BPM
+	if want != got {
+		t.Errorf("want: %f != got: %f", want, got)
 	}
-	if bpmParts[1].BPM != 30. {
-		t.Errorf("want: %f != got: %f", 30., bpmParts[1].BPM)
+	want = 60.
+	got = bpmParts[1].BPM
+	if want != got {
+		t.Errorf("want: %f != got: %f", want, got)
 	}
-	if float64(bpmParts[0].Position) != 0 {
-		t.Errorf("want: %d != got: %d", 0, bpmParts[0].Position)
+	wantPos := 0
+	gotPos := bpmParts[0].Position
+	if gotPos != wantPos {
+		t.Errorf("want: %d != got: %d", wantPos, gotPos)
 	}
-	if float64(bpmParts[1].Position) != 192*4 {
-		t.Errorf("want: %d != got: %d", 192*4, bpmParts[1].Position)
+	wantPos = 192 * 4
+	gotPos = bpmParts[1].Position
+	if gotPos != wantPos {
+		t.Errorf("want: %d != got: %d", wantPos, gotPos)
 	}
 }
 func Test44WithAverage8(t *testing.T) {
 	tapTimes := []float64{
-		1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 14, 16, 18, 20, 22,
+		0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 5., 6., 7., 8., 9., 10., 11., 12.,
 	}
 	chartInfo := ChartInfo{Name: "Name", Artist: "Artist", AverageBeatCount: 8, BeatsPerBar: 4}
 
 	bpmParts := CalculateBPMParts(tapTimes, chartInfo)
 	fmt.Println(bpmParts)
 
-	if bpmParts[0].BPM != 60. {
-		t.Errorf("want: %f != got: %f", 60., bpmParts[0].BPM)
+	want := 120.
+	got := bpmParts[0].BPM
+	if want != got {
+		t.Errorf("want: %f != got: %f", want, got)
 	}
-	if bpmParts[1].BPM != 30. {
-		t.Errorf("want: %f != got: %f", 30., bpmParts[1].BPM)
+	want = 60.
+	got = bpmParts[1].BPM
+	if want != got {
+		t.Errorf("want: %f != got: %f", want, got)
 	}
-
-	if float64(bpmParts[0].Position) != 0 {
-		t.Errorf("want: %d != got: %d", 0, bpmParts[0].Position)
+	wantPos := 0
+	gotPos := bpmParts[0].Position
+	if gotPos != wantPos {
+		t.Errorf("want: %d != got: %d", wantPos, gotPos)
 	}
-	if float64(bpmParts[1].Position) != 192*8 {
-		t.Errorf("want: %d != got: %d", 192*8, bpmParts[1].Position)
+	wantPos = 192 * 8
+	gotPos = bpmParts[1].Position
+	if gotPos != wantPos {
+		t.Errorf("want: %d != got: %d", wantPos, gotPos)
 	}
 }
